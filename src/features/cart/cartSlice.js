e8ed1fb71f8fa647bee83210dcbdfa63cab141f3
Mohammad Mehdi Beheshti <mohammadmehdibeheshti@yahoo.com>
2023-12-12 14:23:21 +0330

    fix: getTotalCartPrice function

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -39,10 +39,10 @@ export const {
   clearCart,
 } = cartSlice.actions;
 export default cartSlice.reducer;
-export const getCart=(state)=>state.cart.cart
+export const getCart = (state) => state.cart.cart;
 export const getUsername = (state) => state.user.username;
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
 export const getTotalCartPrice = (state) =>
-  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+  state.cart.cart.reduce((sum, item) => sum + item.unitPrice, 0);
